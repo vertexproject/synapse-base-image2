f80e1bed9a42c496cecfa9bb1eb58c1929827848
epiphyte <epiphyte@vertex.link>
2018-07-23 15:52:54 -0400

    Bump tornado, make tests us 010 branch. Remove psql instance.

--- test/synapse_test.sh
+++ test/synapse_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 git clone https://github.com/vertexproject/synapse.git  ./synapse
+# XXX Fix this once we mainline 010 :)
+git checkout 010
 cd ./synapse
 pytest -v -s --disable-warnings --durations 6 --maxfail 6 --cov synapse --no-cov-on-fail --cov-report=term
 exit $?
