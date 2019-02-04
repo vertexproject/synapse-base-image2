75d189774ede211680983c429b3ea50eeab6929c
epiphyte <epiphyte@vertex.link>
2019-02-04 21:37:03 +0000

    Remove 010 version reference.

--- test/synapse_test.sh
+++ test/synapse_test.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 git clone https://github.com/vertexproject/synapse.git  ./synapse
 cd ./synapse
-# XXX Fix this once we mainline 010 :)
-git checkout 010
 pytest -v -s --disable-warnings --durations 6 --maxfail 6 --cov synapse --no-cov-on-fail --cov-report=term
 exit $?
